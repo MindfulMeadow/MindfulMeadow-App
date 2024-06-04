4fcb62e6234ac110648f4f5cb8490154506fce1e
lfengzuo <754198932@qq.com>
2024-06-05 00:40:38 +0100

    ADD: implement frontend and part of backend of the first step of start a log, leave the "emotion record" as TODO

--- app/src/main/java/com/group49/mindfulmeadow_app/Logging_fstActivity.kt
+++ app/src/main/java/com/group49/mindfulmeadow_app/Logging_fstActivity.kt
@@ -1,20 +1,71 @@
 package com.group49.mindfulmeadow_app
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Logging_fstActivity : AppCompatActivity() {
+
+    private lateinit var mBtnBackToHome: ImageView
+
+    private lateinit var mBtnJoy: ImageView
+    private lateinit var mBtnGenius: ImageView
+    private lateinit var mBtnDisgust: ImageView
+    private lateinit var mBtnAnger: ImageView
+    private lateinit var mBtnFear: ImageView
+    private lateinit var mBtnSad: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_loggingfst)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        mBtnBackToHome = findViewById(R.id.iv_back_to_home)
+
+        mBtnBackToHome.setOnClickListener {
+            val intent = Intent(this@Logging_fstActivity, HomeActivity::class.java)
+            startActivity(intent)
         }
+
+        mBtnJoy = findViewById(R.id.iv_joy)
+        mBtnGenius = findViewById(R.id.iv_genius)
+        mBtnDisgust = findViewById(R.id.iv_disgust)
+        mBtnAnger = findViewById(R.id.iv_anger)
+        mBtnFear = findViewById(R.id.iv_fear)
+        mBtnSad = findViewById(R.id.iv_sad)
+
+        mBtnJoy.setOnClickListener {
+            //TODO: 记录emotion joy
+            jumpToLoggingSndPage()
+        }
+
+        mBtnGenius.setOnClickListener {
+            //TODO: 记录emotion genius
+            jumpToLoggingSndPage()
+        }
+
+        mBtnDisgust.setOnClickListener {
+            //TODO: 记录emotion disgust
+            jumpToLoggingSndPage()
+        }
+
+        mBtnAnger.setOnClickListener {
+            //TODO: 记录emotion anger
+            jumpToLoggingSndPage()
+        }
+
+        mBtnFear.setOnClickListener {
+            //TODO: 记录emotion fear
+            jumpToLoggingSndPage()
+        }
+
+        mBtnSad.setOnClickListener {
+            //TODO: 记录emotion sad
+            jumpToLoggingSndPage()
+        }
+    }
+
+    private fun jumpToLoggingSndPage() {
+        val intent = Intent(this@Logging_fstActivity, Logging_sndActivity::class.java)
+        startActivity(intent)
     }
 }

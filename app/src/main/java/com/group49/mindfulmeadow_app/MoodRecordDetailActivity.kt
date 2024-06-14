e70da56cf4f23ed7259338bc0ee35da8693deaa4
lfengzuo <754198932@qq.com>
2024-06-14 01:37:16 +0100

    MODIFIED: Rough implementation for history page to display the picture of the user.

--- app/src/main/java/com/group49/mindfulmeadow_app/MoodRecordDetailActivity.kt
+++ app/src/main/java/com/group49/mindfulmeadow_app/MoodRecordDetailActivity.kt
@@ -1,10 +1,12 @@
 package com.group49.mindfulmeadow_app
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.group49.mindfulmeadow_app.DataBase.Companion.downloadFile
 
 class MoodRecordDetailActivity : AppCompatActivity() {
 
@@ -13,8 +15,9 @@ class MoodRecordDetailActivity : AppCompatActivity() {
     private lateinit var tvDescription: TextView
     private lateinit var tvLog: TextView
     private lateinit var tvDate: TextView
+    private lateinit var ivRecordImage: ImageView
 
-    private lateinit var mBtnBackToHistory: ImageView
+    private lateinit var ivBackToHistory: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,20 +28,36 @@ class MoodRecordDetailActivity : AppCompatActivity() {
         tvDescription = findViewById(R.id.tv_description)
         tvLog = findViewById(R.id.tv_log)
         tvDate = findViewById(R.id.tv_date)
-        mBtnBackToHistory = findViewById(R.id.iv_back_to_history)
+        ivBackToHistory = findViewById(R.id.iv_back_to_history)
+        ivRecordImage = findViewById(R.id.iv_record_image)
 
-        mBtnBackToHistory.setOnClickListener {
+        ivBackToHistory.setOnClickListener {
             val intent = Intent(this@MoodRecordDetailActivity, HistoryActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.enter_anim, R.anim.exit_anim )
         }
 
-        val selectedMoodRecord = intent.getSerializableExtra("selectedMoodRecord") as MoodRecord
 
-        tvUserId.text = selectedMoodRecord.userId
-        tvFeeling.text = selectedMoodRecord.feeling
-        tvDescription.text = selectedMoodRecord.description.joinToString(", ")
-        tvLog.text = selectedMoodRecord.log
-        tvDate.text = selectedMoodRecord.date
+        val moodRecord = intent.getSerializableExtra("selectedMoodRecord") as? MoodRecord
+
+        moodRecord?.let {
+            tvUserId.text = it.userId
+            tvDate.text = it.date
+            tvFeeling.text = it.feeling
+            tvDescription.text = it.description.joinToString(", ")
+            tvLog.text = it.log
+
+            if (it.imageUrl.isNotEmpty()) {
+                downloadFile(it.imageUrl, { file ->
+                    ivRecordImage.setImageURI(Uri.fromFile(file))
+                    ivRecordImage.visibility = ImageView.VISIBLE
+                }, {
+                    // Handle failure
+                })
+            } else {
+                ivRecordImage.visibility = ImageView.GONE
+            }
+        }
     }
+
 }
